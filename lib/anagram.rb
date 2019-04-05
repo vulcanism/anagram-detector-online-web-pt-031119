9d5b45e1b0c38aed62d2ee42188e4a9d42375a33
Savannah C <sgcorrell@gmail.com>
2019-04-05 20:20:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def intialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    anagram = []
+    array.select do |given_word|
+      if (given_word.split("").sort) == (@word.split("").sort)
+        anagram << given_word
+      end
+    end
+  end
+end
